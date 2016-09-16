eef5db581e7d579bf6a5cca0dca3dd6acaeb3b3a
Sascha S <cetheus@gmx.net>
2016-09-16 15:58:36 +0200

    Script updated to handle channels by ID
    The Script was updated to handle the Uploads of a Channel by providing the Channel ID.
    Because the YT-API doesn't allow more than 50 Results, I build in a limiter.

--- index.php
+++ index.php
@@ -1,100 +1,168 @@
-<?php header("Content-Type: application/xml");
-
+<?php
 // Config Section
 $apiKey = '';
 
 // Define Variables
 $playlistId = '';
+$channelId = '';
+$maxChannelResults = 20; //Default number of Items requested
 
 if($apiKey == '') {
 	die('Please configure the API-Key');
-} else if(!isset($_GET["playlistid"])) {
-	die('No Playlist ID (playlistid) provided.');
-} else {
+} else if (isset($_GET["playlistid"])) {
 	$playlistId = $_GET["playlistid"];
-}
 
-// API Request for Playlist Properties
-$apiRequest = getdata('https://www.googleapis.com/youtube/v3/playlists?'
+	// API Request for Playlist Properties
+	$apiRequest = getdata('https://www.googleapis.com/youtube/v3/playlists?'
 			. 'part=snippet'
-			. '&id=' . $playlistId 
+			. '&id=' . $playlistId
 			. '&key=' . $apiKey);
-
-$playlist = json_decode($apiRequest, true);
-
-// DIE if $video is empty
-if(!isset($playlist) OR !$playlistId){
-	die('Could not load Playlist');
-}
-
-// RSS output
-echo '<?xml version="1.0" encoding="UTF-8" ?>' . "\n"
+	
+	$playlist = json_decode($apiRequest, true);
+	
+	// DIE if $video is empty
+	if(!isset($playlist)){
+		die('Could not load Playlist');
+	}
+	
+	// Define Header
+	header("Content-Type: application/rss+xml");
+	
+	// RSS output
+	echo '<?xml version="1.0" encoding="UTF-8" ?>' . "\n"
 		. '<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">' . "\n"
 			. '<channel>' . "\n"
 				. '<title>' . $playlist["items"][0]["snippet"]["title"] . '</title>' . "\n"
 				. '<link>https://www.youtube.com/playlist?list=' . $playlist["items"][0]["id"] . '</link>' . "\n"
 				. '<description>' . $playlist["items"][0]["snippet"]["description"] . '</description>' . "\n"
 				. '<image>'  . "\n"
-						. '<url>'. $playlist["items"][0]["snippet"]["thumbnails"]["medium"]["url"] . '</url>' . "\n"
-						. '<title>' . $playlist["items"][0]["snippet"]["title"] . '</title>' . "\n"
-						. '<link>https://www.youtube.com/playlist?list=' . $playlist["items"][0]["id"] . '</link>' . "\n"
+					. '<url>'. $playlist["items"][0]["snippet"]["thumbnails"]["medium"]["url"] . '</url>' . "\n"
+					. '<title>' . $playlist["items"][0]["snippet"]["title"] . '</title>' . "\n"
+					. '<link>https://www.youtube.com/playlist?list=' . $playlist["items"][0]["id"] . '</link>' . "\n"
 				. '</image>' . "\n"
 				. '<atom:link href="http://'
 					. $_SERVER['HTTP_HOST']
 					.$_SERVER['PHP_SELF']
 					. '?playlistid=' . $playlistId
 					.'" rel="self" type="application/rss+xml" />' . "\n";
+	
+	unset($apiRequest);
 
-unset($apiRequest);
-
-// API Request for Playlist Items
-$apiRequest = getdata('https://www.googleapis.com/youtube/v3/playlistItems?'
+	// API Request for Playlist Items
+	$apiRequest = getdata('https://www.googleapis.com/youtube/v3/playlistItems?'
 			. 'part=snippet'
-			. '&playlistId=' . $playlistId 
+			. '&playlistId=' . $playlistId
 			. '&maxResults=10'
 			. '&fields=items%2Fsnippet'
 			. '&key=' . $apiKey);
 
-$video = json_decode($apiRequest, true);
+	$video = json_decode($apiRequest, true);
 
-// DIE if $video is empty
-if(!isset($video)){
-	die('No Video Data available');
-}
-
-// Count Videos
-if(count($video['items'])>0) {
+	// DIE if $video is empty
+	if(!isset($video)){
+		die('No Video Data available');
+	}
+	
 	//Schleife durch $video Array beginnend bei items
 	foreach($video['items'] as $video_data) {
-		if($video_data['snippet']['title'] != "Private video"){
-			echo '<item>' . "\n"
+		echo 	'<item>' . "\n"
 					. '<title>' . $video_data['snippet']['title'] .'</title>' . "\n"
-					. '<link>https://www.youtube.com/watch?v=' 
-						. $video_data['snippet']['resourceId']['videoId'] 
+					. '<link>https://www.youtube.com/watch?v='
+						. $video_data['snippet']['resourceId']['videoId']
 					. '</link>' . "\n"
-					. '<description><![CDATA[' 
-						. '<img src="' . $video_data['snippet']['thumbnails']['medium']['url'] . '" '
-							. 'alt="' . $video_data['snippet']['title'] . '" '
-							. 'width="'. $video_data['snippet']['thumbnails']['medium']['width'] . '" '
-							. 'height="' . $video_data['snippet']['thumbnails']['medium']['height'] . '" '
-							. '/>' 
-						. '<br />' 
-						. $video_data['snippet']['description'] 
-					. ']]></description>' . "\n"
-					. '<guid>https://www.youtube.com/watch?v=' 
-						. $video_data['snippet']['resourceId']['videoId'] 
+					. '<description></description>' . "\n"
+					. '<guid>https://www.youtube.com/watch?v='
+						. $video_data['snippet']['resourceId']['videoId']
 					. '</guid>' . "\n"
-					. '<pubDate>' 
-						. date(DATE_RFC2822, strtotime($video_data['snippet']['publishedAt']))
-					. '</pubDate>' . "\n"
-				. '</item>' . "\n"; 
-		}
+				. '</item>' . "\n";
 	} // Ende Foreach Schleife
-} // End Count Videos
 
-// RSS schließen
-echo '</channel>' . "\n"
+	// RSS schließen
+	echo '</channel>' . "\n"
 		. '</rss>';
+
+} else if (isset($_GET["channelid"])) {
+	if (isset($_GET["maxchannelresults"])){
+		$maxChannelResults = $_GET["maxchannelresults"];
+		
+		if ($maxChannelResults > 50) {
+			$maxChannelResults = 50;
+		}
+	}
+	$channelId = $_GET["channelid"];
+
+	// API Request for Chanel Properties
+	$apiRequest = getdata('https://www.googleapis.com/youtube/v3/channels?'
+			. 'part=brandingSettings'
+			. '&id=' . $channelId
+			. '&key=' . $apiKey);
+	
+	$channel = json_decode($apiRequest, true);
+	
+	// DIE if $channel is empty
+	if(!isset($channel)){
+		die('Could not load Channel Data');
+	}
+	
+	// Define Header
+	header("Content-Type: application/rss+xml");
+	
+	// RSS output
+	echo '<?xml version="1.0" encoding="UTF-8" ?>' . "\n"
+		. '<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">' . "\n"
+			. '<channel>' . "\n"
+				. '<title>' . $channel["items"][0]["brandingSettings"]["channel"]["title"] . '</title>' . "\n"
+				. '<link>https://www.youtube.com/channel/' . $channel["items"][0]["id"] . '</link>' . "\n"
+				. '<description>' . $channel["items"][0]["brandingSettings"]["channel"]["description"] . '</description>' . "\n"
+				. '<image>'  . "\n"
+					. '<url>'. $channel["items"][0]["brandingSettings"]["image"]["bannerImageUrl"] . '</url>' . "\n"
+					. '<title>' . $channel["items"][0]["brandingSettings"]["channel"]["title"] . '</title>' . "\n"
+					. '<link>https://www.youtube.com/channel/' . $channel["items"][0]["id"] . '</link>' . "\n"
+				. '</image>' . "\n"
+				. '<atom:link href="http://'
+					. $_SERVER['HTTP_HOST']
+					.$_SERVER['PHP_SELF']
+					. '?channelid=' . $channelId
+					.'" rel="self" type="application/rss+xml" />' . "\n";
+	
+	unset($apiRequest);
+
+	// API Request for Playlist Items
+	$apiRequest = getdata('https://www.googleapis.com/youtube/v3/search?'
+			. 'part=snippet,id'
+			. '&order=date'
+			. '&maxResults=' . $maxChannelResults
+			. '&channelId=' . $channelId
+			. '&key=' . $apiKey);
+
+	$video = json_decode($apiRequest, true);
+
+	// DIE if $video is empty
+	if(!isset($video)){
+		die('No Video Data available');
+	}
+	
+	//Schleife durch $video Array beginnend bei items
+	foreach($video['items'] as $video_data) {
+		echo 	'<item>' . "\n"
+					. '<title>' . $video_data['snippet']['title'] .'</title>' . "\n"
+					. '<link>https://www.youtube.com/watch?v='
+						. $video_data['id']['videoId']
+					. '</link>' . "\n"
+					. '<description>' . $video_data['snippet']['description'] .'</description>' . "\n"
+					. '<guid>https://www.youtube.com/watch?v='
+						. $video_data['id']['videoId']
+					. '</guid>' . "\n"
+				. '</item>' . "\n";
+	} // Ende Foreach Schleife
+
+	// RSS schließen
+	echo '</channel>' . "\n"
+		. '</rss>';
+
+} else if(!isset($_GET["playlistid"]) && !isset($_GET["channelid"])) {
+	die('No Playlist ID (playlistid) or Channel ID (channelid) provided.');
+}
 
 // Functions
 function getdata($url) {
